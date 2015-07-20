6f7fcae7317e292076b7f6d6aff9c558997d60b0
C.K. Chan <chanchikin@ied.edu.hk>
2015-07-20 11:17:10 +0800

    Topic with category filter

--- libraries/Topic.php
+++ libraries/Topic.php
@@ -36,6 +36,84 @@ class Topic{
 	return $results;
 				 
 	}
+	
+	/*
+	 * Get total # of topics
+	 */
+	
+	public function getTotalTopics(){
+		$this->db->query("SELECT * FROM topics");
+		$rows = $this->db->resultset();
+		return $this->db->rowCount();
+		
+	}
+	/*
+	 * Get Total # of categories
+	 */
+	
+	public function getTotalCategories(){
+		$this->db->query("SELECT * FROM categories");
+		$rows = $this->db->resultset();
+		return $this->db->rowCount();
+		
+	}
+
+	/*
+	 * Get total # of replies
+	 */
+	
+	public function getTotalReplies($topic_id){
+		$this->db->query('SELECT * FROM replies WHERE topic_id='.$topic_id);
+		$rows = $this->db->resultset();
+		return $this->db->rowCount;
+		
+	}
+	
+	/*
+	 * Get category BY ID
+	 * 
+	 */
+	public function getCategory($category_id){
+		$this->db->query("SELECT * FROM categories WHERE id = :category_id
+		");
+		$this->db->bind(':category_id', $category_id);
+	
+		//Assign Row
+		$row = $this->db->single();
+	
+		return $row;
+	}
+	
+	/*
+	 * Get Topics By Category
+	 */
+	public function getByCategory($category_id){
+	$this->db->query("SELECT topics.*, categories.*, users.username, users.avatar FROM topics
+						INNER JOIN categories
+						ON topics.category_id = categories.id
+						INNER JOIN users
+						ON topics.user_id=users.id
+						WHERE topics.category_id = :category_id	
+					
+				");
+	$this->db->bind(':category_id', $category_id);
+	
+	/*
+	 * SELECT topics.*, categories.*,users.username, users.avatar, categories.name FROM topics
+							INNER JOIN users
+							ON topics.user_id = users.id
+							INNER JOIN categories
+							ON topics.category_id = categories.id
+							ORDER BY create_date DESC
+							WHERE topics.category_id = :category_id
+	 */
+	
+	//Assign Result Set
+	$results = $this->db->resultset();
+	return $results;
+	
+	}
+	
 }
 
 
