80f2147d5ac304711110d041c487e9c1e5eadfa2
C.K. Chan <chanchikin@ied.edu.hk>
2015-07-23 16:57:52 +0800

    Single Topic Page Template

--- topics.php
+++ topics.php
@@ -4,11 +4,44 @@
 //Create Topic Object
 $topic = new Topic();
 
+//Get categorhy from URL
+$category = isset($_GET['category']) ? $_GET['category']: null;
+
+//Get user from URL
+//$user_id = isset($_GET['user']) ? $_GET['user']: null;
+$user_id = isset($_GET['user']) ? $_GET['user'] : null;
+
 //Get Template and Assign vars
 $template = new Template('templates/topics.php');
 
 //Assign Vars
 
+if (isset($category)) {
+	$template->topics = $topic->getByCategory($category);
+	$template->title = 'Posts In "'.$topic->getCategory($category)->name.'"';
+	
+}
+
+//Check for user filter
+// if(isset($user_id)){
+// 	$template->topics = $topic->getByUser($user_id);
+// 	//$template->title = 'Posts By "'.$user->getUser($user_id)->username.'"';
+	
+// }
+
+if(isset($user_id)){
+	$template->topics = $topic->getByUser($user_id);
+	//$template->title = 'Posts By "'.$user->getUser($user_id)->username.'"';
+	echo("checked");
+}
+
+if(!isset($category) && !isset($user_id)){
+	$template->topics = $topic->getAllTopics();
+}
+
+
+$template->totalTopics = $topic->getTotalTopics();
+$template->totalCategories = $topic->getTotalCategories();
 //Display template
 echo $template;
 
