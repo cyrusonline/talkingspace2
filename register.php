aeda8fe1e50a8e0411b183f319bfb7af0e40b9cb
C.K. Chan <chanchikin@ied.edu.hk>
2015-07-24 11:13:45 +0800

    The user class 11:12

--- register.php
+++ register.php
@@ -1,7 +1,34 @@
 <?php require 'core/init.php'; ?>
 
 <?php 
+//Create Topic Object
+$topic = new Topic();
+//Create User Object
+$user = new User();
 
+
+if (isset($_POST['register'])) {
+	//Create Daata Array
+	$data = array();
+	$data['name']=$_POST['name'];
+	$data['email']=$_POST['email'];
+	$data['username']=$_POST['username'];
+	$data['password']=md5($_POST['password']);
+	$data['password2']=md5($_POST['password2']);
+	$data['about']=$_POST['about'];
+	
+	$data['last_activity']=date("Y-m-d H:i:s");
+	
+	//Upload Avatar Image
+	if ($user->uploadAvatar()) {
+		
+		$data['avatar']=$_FILES["avatar"]["name"];
+			
+	}else{
+		$data['avatar'] = 'noimage.png';
+	}
+	
+}
 //Get Template and Assign vars
 $template = new Template('templates/register.php');
 
