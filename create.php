d8819f4bd9179481ae6e5f3d3d19662b67ba1dac
C.K. Chan <chanchikin@ied.edu.hk>
2015-07-30 17:02:15 +0800

    adding topic 9:19

--- create.php
+++ create.php
@@ -1,6 +1,23 @@
 <?php require 'core/init.php'; ?>
 
 <?php 
+//Create Topic Object
+$topic = new Topic();
+
+if (isset($_POST['do_create'])){
+	//create Data Array
+	$data = array();
+	$data['title'] = $_POST['title'];
+	$data['body'] = $_POST['body'];
+	$data['category_id'] = $_POST['category'];
+	$data['user_id'] = getUser()['user_id'];
+	$data['last_activity'] = date("Y-m-d H:i:s");
+	
+	
+	
+	
+}
+
 
 //Get Template and Assign vars
 $template = new Template('templates/create.php');
