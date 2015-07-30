05295f715c3dda4129a25f80977ef757813c661b
C.K. Chan <chanchikin@ied.edu.hk>
2015-07-30 12:15:11 +0800

    adding topic 9:14

--- templates/create.php
+++ templates/create.php
@@ -1,16 +1,16 @@
 <?php include 'includes/header.php';?>
-		<form role="form">
+		<form role="form" method="post" action="create.php">
 						<div class="form-group">
 							<label>Topic title</label> <input type="text"
 								class="form-control" name="title" placeholder="Enter Post Title">
 						</div>
 						<div class="form-group">
-							<label>Category</label> <select class="form-control">
-								<option>Design</option>
-								<option>Development</option>
-								<option>Business & Marketing</option>
-								<option>Search Engines</option>
-								<option>Cloud & Hosting</option>
+							<label>Category</label>
+							 <select class="form-control" name = "category">
+								<?php foreach(getCategories() as $category) : ?>
+								<option value = "<?php echo $category->id;?>"><?php echo $category->name;?>
+								<?php endforeach;?>
+								
 							</select>
 						</div>
 						<div class="form-group">
@@ -18,7 +18,7 @@
 						<textarea id="body" rows="10" cols="80" class="form-control" name="body"></textarea>
 						<script>CKEDITOR.replace('body');</script>	
 						</div>
-						<button type="submit" class="btn btn-default">Submit</button>
+						<button name="do_create" type="submit" class="btn btn-default">Submit</button>
 
 					</form>
 <?php include 'includes/footer.php';?>
